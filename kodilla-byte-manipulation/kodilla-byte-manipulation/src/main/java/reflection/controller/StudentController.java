890dafc78affa9e51214be76817006d06e3e6eca
Andrzej Rzeszutek <andrzej.rzeszutek@gmail.com>
2024-06-05 22:39:55 +0200

    4.3 Student Validator

--- kodilla-byte-manipulation/kodilla-byte-manipulation/src/main/java/reflection/controller/StudentController.java
+++ kodilla-byte-manipulation/kodilla-byte-manipulation/src/main/java/reflection/controller/StudentController.java
@@ -1,35 +1,34 @@
 package reflection.controller;
 
-import org.springframework.web.bind.annotation.RestController;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.validation.annotation.Validated;
+import org.springframework.web.bind.annotation.*;
 
 import java.lang.reflect.Field;
-import java.util.Map;
 import java.util.HashMap;
+import java.util.Map;
+
 import reflection.Student;
+import reflection.validation.Range;
 
-
+@Validated
 @RestController
 @RequestMapping("/v1/students")
 public class StudentController {
 
     @PostMapping("/generate")
-    public Map<Integer, String> generateStudents(@RequestParam(defaultValue = "20") int n,
-                                                 @RequestParam(defaultValue = "10") int z) throws NoSuchFieldException, IllegalAccessException {
+    public Map<Integer, String> generateStudents(
+            @RequestParam(defaultValue = "20") @Range(min = 1, max = 100) int n,
+            @RequestParam(defaultValue = "10") @Range(min = 1, max = 20) int z) throws NoSuchFieldException, IllegalAccessException {
         Map<Integer, String> resultMap = new HashMap<>();
 
         Field indexField = Student.class.getDeclaredField("indexNumber");
         indexField.setAccessible(true);
 
         for (int i = 0; i < n; i++) {
-
             Student student = new Student(z);
-            String indexNumber = (String)indexField.get(student);
+            String indexNumber = (String) indexField.get(student);
             resultMap.put(System.identityHashCode(student), indexNumber);
         }
         return resultMap;
     }
-
 }
---
+++ kodilla-byte-manipulation/kodilla-byte-manipulation/src/main/java/reflection/validation/RangeValidator.java
@@ -0,0 +1,24 @@
+package reflection.validation;
+
+import jakarta.validation.ConstraintValidator;
+import jakarta.validation.ConstraintValidatorContext;
+
+public class RangeValidator implements ConstraintValidator<Range, Integer> {
+
+    private int min;
+    private int max;
+
+    @Override
+    public void initialize(Range range) {
+        this.min = range.min();
+        this.max = range.max();
+    }
+
+    @Override
+    public boolean isValid(Integer value, ConstraintValidatorContext context) {
+        if (value == null) {
+            return true;
+        }
+        return value >= min && value <= max;
+    }
+}
